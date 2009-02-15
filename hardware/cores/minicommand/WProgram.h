7d48c071d7f00bd2426b81de27f0ee4b4d81e38d
wesen3000 <wesen3000@6f8c04f6-b8b6-11de-9846-1b058f2a8eb1>
2009-02-15 14:49:46 +0000

    remove wmath functions and move them to commontools library
    git-svn-id: https://mididuino.googlecode.com/svn/trunk@118 6f8c04f6-b8b6-11de-9846-1b058f2a8eb1

--- hardware/cores/minicommand/WProgram.h
+++ hardware/cores/minicommand/WProgram.h
@@ -20,19 +20,8 @@ extern "C" {
 #include "Stack.h"
 #include "GUI.h"
 #include "Midi.h"
+#include "WMath.h"
 
-uint16_t makeWord(uint16_t w);
-uint16_t makeWord(uint8_t h, uint8_t l);
-
-#define word(...) makeWord(__VA_ARGS__)
-
-unsigned long pulseIn(uint8_t pin, uint8_t state, unsigned long timeout = 1000000L);
-
-// WMath prototypes
-long random(long);
-long random(long, long);
-void randomSeed(unsigned int);
-long map(long, long, long, long, long);
 #endif
 
 /* default config flags */
