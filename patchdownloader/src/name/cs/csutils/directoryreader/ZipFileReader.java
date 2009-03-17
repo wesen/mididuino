445e09cf06a23c5ecaa86083c958723c2fb5027b
chresan <chresan@6f8c04f6-b8b6-11de-9846-1b058f2a8eb1>
2009-03-17 17:19:55 +0000

    fixed some path seperator issues, new license
    git-svn-id: https://mididuino.googlecode.com/svn/trunk@179 6f8c04f6-b8b6-11de-9846-1b058f2a8eb1

--- patchdownloader/src/name/cs/csutils/directoryreader/ZipFileReader.java
+++ patchdownloader/src/name/cs/csutils/directoryreader/ZipFileReader.java
@@ -1,21 +1,30 @@
-/** 
- * Copyright (C) 2009 Christian Schneider
- *
- * This file is part of Patchdownloader.
+/**
+ * Copyright (c) 2009, Christian Schneider
+ * All rights reserved.
  * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; version 2
- * of the License.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * - Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *  - Neither the names of the authors nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
  * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 package name.cs.csutils.directoryreader;
 
@@ -25,6 +34,8 @@ import java.util.Enumeration;
 import java.util.NoSuchElementException;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
+
+import name.cs.csutils.CSUtils;
 
 /**
  * Reads files and directories in a zip file. 
@@ -49,6 +60,14 @@ public class ZipFileReader<Z extends ZipFile> extends DirectoryReader {
 
     public Z getZipFile() {
         return zipFile;
+    }
+
+    /**
+     * Returns the path inside the zip file.
+     */
+    @Override
+    protected String resolvePath(String path) {
+        return zipFile.getName()+"!"+path;
     }
     
     /**
@@ -101,9 +120,9 @@ public class ZipFileReader<Z extends ZipFile> extends DirectoryReader {
                 if ((basePath.length() == 0 || basePath.equals("/"))) {
                     // the condition indicates that this is the root path name
                     basePath = null; 
-                } else if (!basePath.endsWith("/")) {
+                } else if (basePath.endsWith("/")) {
                     // ensure that the path name ends with a slash
-                    basePath = basePath + '/';
+                    basePath = basePath.substring(0, basePath.length()-1);
                 }
             }
             
@@ -130,15 +149,24 @@ public class ZipFileReader<Z extends ZipFile> extends DirectoryReader {
                     return true;
                 } 
                 
-                if (entryPath.startsWith(basePath)) {                    
+                if (entryPath.startsWith(basePath+'/')) {                    
                     // translate path to base path
                     if (relativePaths) {
                         
                         if (entryPath.length() == basePath.length()) {
-                            entryPath = getLastPathComponent(entryPath);
+                            entryPath = CSUtils.getLastPathComponent(entryPath);
                         } else {
-                            entryPath = entryPath.substring(basePath.length());
+                            entryPath = entryPath.substring(basePath.length()+1);
                         }
+                    }
+                    
+                    // finished
+                    next = new XZipEntry(entryPath, zipEntry);
+                } else if (entryPath.equals(basePath)) {                    
+                    // translate path to base path
+                    if (relativePaths) {
+                        
+                        entryPath = CSUtils.getLastPathComponent(entryPath);
                     }
                     
                     // finished
@@ -163,18 +191,10 @@ public class ZipFileReader<Z extends ZipFile> extends DirectoryReader {
         
     }
     
-    /**
-     * Returns the last path component of the specified path.
-     * The components are separated using the slash character '/'
-     * @param path the path
-     * @return the last path component of the specified path
-     */
-    static String getLastPathComponent(String path) {
-        int separator = path.lastIndexOf('/');
-        if (separator>0) { // keep name if it contains only a single '/'
-            return path.substring(separator+1);
-        }
-        return path;
+    @Override
+    public void close() throws IOException {
+        zipFile.close();
     }
+    
 
 }
