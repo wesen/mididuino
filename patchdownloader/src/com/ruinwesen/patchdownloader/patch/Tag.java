fa9ca39afcea0d47f61fe4982cdd0265cfaf6d53
chresan <chresan@6f8c04f6-b8b6-11de-9846-1b058f2a8eb1>
2009-03-20 21:20:36 +0000

    new javadoc comments
    git-svn-id: https://mididuino.googlecode.com/svn/trunk@225 6f8c04f6-b8b6-11de-9846-1b058f2a8eb1

--- patchdownloader/src/com/ruinwesen/patchdownloader/patch/Tag.java
+++ patchdownloader/src/com/ruinwesen/patchdownloader/patch/Tag.java
@@ -28,12 +28,32 @@
  */
 package com.ruinwesen.patchdownloader.patch;
 
-public final class Tag implements Comparable<Tag> {
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
+/**
+ * A tag.
+ * 
+ * @author Christian Schneider
+ */
+public final class Tag implements Comparable<Tag>, Serializable {
+
+    private static final long serialVersionUID = 2469660950046706553L;
+
+    /** the not-normalized tag value */
     private String value;
+    
+    /** the normalized tag value */
     private transient String normalized;
-    private transient int hashCode = 0;
    
+    /** 
+     * A new tag.
+     * @param value the tag value
+     * @throws IllegalArgumentException if the specified argument is <code>null</code>
+     * of the {@link String#length() string-length} of the value is <code>0</code> 
+     */
     public Tag(String value) {
         if (value == null || value.trim().length() == 0) {
             throw new IllegalArgumentException("invalid tag: "+value);
@@ -41,22 +61,42 @@ public final class Tag implements Comparable<Tag> {
         this.value = value;
     }
     
+    /**
+     * Creates a new tag instance with a normalized tag value.
+     * @return a new tag instance with a normalized tag value
+     */
     public Tag normalize() {
         String n = normalizedValue();
         Tag tag = new Tag(n);
         tag.normalized = n;
-        tag.hashCode = hashCode;
         return tag;
     }
     
+    /**
+     * Returns the not-normalized tag value.
+     * @return the not-normalized tag value
+     */
     public String value() {
         return value;
     }
     
+    /**
+     * Normalizes the specified tag value.
+     * @param str a tag
+     * @return the normalized tag
+     * @see #normalizedValue()
+     */
     static String normalizedValue(String str) {
         return str.trim().toLowerCase();
     }
     
+    /**
+     * Returns the normalized tag value.
+     * The normalized tag value is in lowercase letters only and 
+     * does not start / end with whitespace characters.
+     * 
+     * @return the normalized tag value
+     */
     public String normalizedValue() {
         if (normalized == null) {
             normalized = normalizedValue(value);
@@ -67,19 +107,27 @@ public final class Tag implements Comparable<Tag> {
         return normalized;
     }
 
+    /**
+     * Compares this tag's normalized value to the normalized value of the
+     * specified tag lexicographically. 
+     * @see String#compareTo(String)
+     */
     @Override
     public int compareTo(Tag o) {
         return o == this ? 0 : normalizedValue().compareTo(o.normalizedValue());
     }
-    
+
+    /**
+     * Returns a hash code value for the object. 
+     */
     @Override
     public int hashCode() {
-        if (hashCode == 0) {
-            this.hashCode = normalizedValue().hashCode();
-        }
-        return hashCode;
+        return normalizedValue().hashCode();
     }
     
+    /**
+     * Indicates whether some other object is "equal to" this one.
+     */
     @Override
     public boolean equals(Object o) {
         return o == this 
@@ -87,9 +135,22 @@ public final class Tag implements Comparable<Tag> {
             && normalizedValue().equalsIgnoreCase(((Tag)o).normalizedValue());
     }
     
+    /**
+     * Returns the tag value.
+     * @return the tag value
+     */
     @Override
     public String toString() {
         return value;
     }
-    
+
+    private void writeObject(ObjectOutputStream oos) throws IOException {
+        oos.defaultWriteObject();
+    }
+
+    private void readObject(ObjectInputStream ois) 
+        throws ClassNotFoundException, IOException {
+        ois.defaultReadObject();
+        this.normalized = null;
+    }
 }
