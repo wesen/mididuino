375e0c5d05d5d54812ead20cac91cc703f1a7861
wesen3000 <wesen3000@6f8c04f6-b8b6-11de-9846-1b058f2a8eb1>
2009-05-28 06:38:32 +0000

    added build script for mididuino arduino release
    git-svn-id: https://mididuino.googlecode.com/svn/trunk@333 6f8c04f6-b8b6-11de-9846-1b058f2a8eb1

---
+++ build/arduino-libs/dist.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+rm -rf tmp
+
+mkdir tmp
+cp -r ../../hardware/arduino-libraries/MidiUart tmp
+for i in LFO MD Midi MidiTools Sequencer Commontools; do
+    cp -r ../../hardware/libraries/$i tmp
+done
+find tmp -name .svn | xargs rm -rf
+cp -r ../../hardware/arduino-libraries/README.txt tmp
+
+cd tmp
+zip -r MidiDuino-arduino.zip *
+mv MidiDuino-arduino.zip ..
+cd ..
+
+rm -rf tmp
+mkdir tmp
+cp -r ../../hardware/cores/arduinomidi tmp
+cp -r ../../hardware/arduino-libraries/boards.txt tmp
+cp -r ../../hardware/arduino-libraries/README-core.txt tmp
+find tmp -name .svn | xargs rm -rf
+cd tmp
+zip -r MidiDuino-arduinomidi-core.zip *
+mv MidiDuino-arduinomidi-core.zip ..
+cd ..
+
+rm -rf tmp
