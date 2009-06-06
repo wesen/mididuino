450b986708389fa6c7cf369223ffbc3e37ac6b10
wesen3000 <wesen3000@6f8c04f6-b8b6-11de-9846-1b058f2a8eb1>
2009-06-06 18:06:39 +0000

    added lcd parent class to gui
    git-svn-id: https://mididuino.googlecode.com/svn/trunk@388 6f8c04f6-b8b6-11de-9846-1b058f2a8eb1

---
+++ hardware/libraries/GUI/LCDParent.hh
@@ -0,0 +1,59 @@
+#ifndef LCDPARENT_H__
+#define LCDPARENT_H__
+
+#include <avr/pgmspace.h>
+#include <inttypes.h>
+
+class LCDParentClass {
+protected:
+  virtual void enable() { }
+  virtual void putnibble(uint8_t nibble) { }
+  virtual void putcommand(uint8_t command) { }
+  virtual void putdata(uint8_t data) { }
+
+  public:
+  void line1();
+  void line1(char *s);
+  void line1_fill(char *s);
+  void line1_p(PGM_P s);
+  void line1_p_fill(PGM_P s);
+  void line2();
+  void line2(char *s);
+  void line2_fill(char *s);
+  void line2_p(PGM_P s);
+  void line2_p_fill(PGM_P s);
+
+  inline void goLine(uint8_t line) {
+    switch (line) {
+    case 0:
+      line1();
+      break;
+    case 1:
+      line2();
+      break;
+    }
+  }
+  
+  void clearLine();
+
+  void puts(char *s);
+  void puts_fill(char *s, uint8_t i);
+  void puts_fill(char *s);
+  void puts_p(PGM_P s);
+  void puts_p_fill(PGM_P s, uint8_t i);
+  void puts_p_fill(PGM_P s);
+  
+  void put(char *data, uint8_t cnt);
+
+  void putnumber(uint8_t num);
+  void putnumber16(uint16_t num);
+  void putcx(uint8_t i);
+  void putnumberx(uint8_t num);
+  void putnumberx16(uint16_t num);
+  void putnumber32(uint32_t num);
+
+  void blinkCursor(bool blink);
+  void moveCursor(uint8_t row, uint8_t column);
+};
+
+#endif /* LCDPARENT_H__ */
