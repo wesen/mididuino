abe79d890b181dc7613739a6bd351b0fec8796de
wesen3000 <wesen3000@6f8c04f6-b8b6-11de-9846-1b058f2a8eb1>
2009-06-23 19:01:00 +0000

    added events polling and ring buffer for gui events
    git-svn-id: https://mididuino.googlecode.com/svn/trunk@433 6f8c04f6-b8b6-11de-9846-1b058f2a8eb1

---
+++ hardware/libraries/GUI/Events.hh
@@ -0,0 +1,19 @@
+#ifndef EVENTS_H__
+#define EVENTS_H__
+
+#define EVENT_BUTTON_PRESSED  _BV(0)
+#define EVENT_BUTTON_RELEASED _BV(1)
+
+#define EVENT_PRESSED(event, button) ((event).mask & EVENT_BUTTON_PRESSED && (event).source == button)
+#define EVENT_RELEASED(event, button) ((event).mask & EVENT_BUTTON_RELEASED && (event).source == button)
+
+typedef struct gui_event_s {
+  uint8_t mask;
+  uint8_t source;
+} gui_event_t;
+
+extern CRingBuffer<gui_event_t, 8> EventRB;
+
+void pollEventGUI();
+
+#endif /* EVENTS_H__ */
