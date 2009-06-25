3b7e9b717396835983ee70e9e66b8ee91a340a62
wesen3000 <wesen3000@6f8c04f6-b8b6-11de-9846-1b058f2a8eb1>
2009-06-25 13:37:05 +0000

    added callback and destroy method to MDTask
    git-svn-id: https://mididuino.googlecode.com/svn/trunk@458 6f8c04f6-b8b6-11de-9846-1b058f2a8eb1

--- hardware/libraries/MD/MDTask.hh
+++ hardware/libraries/MD/MDTask.hh
@@ -1,7 +1,11 @@
 #ifndef MD_TASK_H__
 #define MD_TASK_H__
 
+#include "Vector.hh"
+
 extern MDClass MD;
+
+typedef void(*md_task_callback_t)();
 
 class MDTaskClass : 
   public Task {
@@ -19,7 +23,56 @@ public:
     reloadGlobal = false;
   }
 
-  void setup();
+  Vector<md_task_callback_t, 4>kitChangeCallbacks;
+  Vector<md_task_callback_t, 4>globalChangeCallbacks;
+  Vector<md_task_callback_t, 4>patternChangeCallbacks;
+
+  void addOnKitChangeCallback(md_task_callback_t cb) {
+    kitChangeCallbacks.add(cb);
+  }
+  void removeOnKitChangeCallback(md_task_callback_t cb) {
+    kitChangeCallbacks.remove(cb);
+  }
+  void addOnGlobalChangeCallback(md_task_callback_t cb) {
+    globalChangeCallbacks.add(cb);
+  }
+  void removeOnGlobalChangeCallback(md_task_callback_t cb) {
+    globalChangeCallbacks.remove(cb);
+  }
+  void addOnPatternChangeCallback(md_task_callback_t cb) {
+    patternChangeCallbacks.add(cb);
+  }
+  void removeOnPatternChangeCallback(md_task_callback_t cb) {
+    patternChangeCallbacks.remove(cb);
+  }
+
+  void callKitCallbacks() {
+    for (int i = 0; i < kitChangeCallbacks.size; i++) {
+      if (kitChangeCallbacks.arr[i] != NULL) {
+	kitChangeCallbacks.arr[i]();
+      }
+    }
+  }
+
+  void callGlobalCallbacks() {
+    for (int i = 0; i < globalChangeCallbacks.size; i++) {
+      if (globalChangeCallbacks.arr[i] != NULL) {
+	globalChangeCallbacks.arr[i]();
+      }
+    }
+  }
+  
+  void callPatternCallbacks() {
+    for (int i = 0; i < patternChangeCallbacks.size; i++) {
+      if (patternChangeCallbacks.arr[i] != NULL) {
+	patternChangeCallbacks.arr[i]();
+      }
+    }
+  }
+  
+  
+  void setup(uint16_t interval = 3000, bool autoLoadKit = false, bool autoLoadGlobal = true,
+	     bool reloadGlobal = true);
 
   virtual void run() {
     MD.sendRequest(MD_STATUS_REQUEST_ID, MD_CURRENT_KIT_REQUEST);
@@ -28,6 +81,8 @@ public:
   }
 
   void onStatusResponse(uint8_t type, uint8_t value);
+
+  virtual void destroy();
 };
 
 
