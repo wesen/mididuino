0354430775a4aa99ab3ebd2f204aaf331b52377c
wesen3000 <wesen3000@6f8c04f6-b8b6-11de-9846-1b058f2a8eb1>
2009-07-02 10:10:29 +0000

    empty implementation placeholder for MonoMachine library
    git-svn-id: https://mididuino.googlecode.com/svn/trunk@493 6f8c04f6-b8b6-11de-9846-1b058f2a8eb1

---
+++ hardware/libraries/MNM/MNM.h
@@ -0,0 +1,44 @@
+#ifndef MNM_H__
+#define MNM_H__
+
+#include <inttypes.h>
+
+#include "WProgram.h"
+#include "Elektron.hh"
+
+extern uint8_t monomachine_sysex_hdr[6];
+
+class MNMEncoder : public CCEncoder {
+};
+
+class MNMClass {
+ public:
+  MNMClass() {
+    baseChannel = 0;
+  }
+
+  uint8_t baseChannel;
+  uint8_t multiTrigChannel;
+  uint8_t autoChannel;
+  
+  int currentGlobal;
+  int currentKit;
+  int currentPattern;
+
+  void loadGlobal(uint8_t id);
+  void loadKit(uint8_t id);
+  void loadPattern(uint8_t id);
+  void loadSong(uint8_t id);
+
+  void setCurrentKitName(char *name);
+
+  void saveKit(uint8_t id);
+
+  void requestKit(uint8_t kit);
+  void requestPattern(uint8_t pattenr);
+  void requestSong(uint8_t song);
+  
+};
+  
+
+    
