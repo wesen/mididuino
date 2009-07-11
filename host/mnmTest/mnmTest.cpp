27f679f4cc965a1323e50085c34c2bf914ebb068
wesen3000 <wesen3000@6f8c04f6-b8b6-11de-9846-1b058f2a8eb1>
2009-07-11 15:23:00 +0000

    added hexdump and helpers
    git-svn-id: https://mididuino.googlecode.com/svn/trunk@520 6f8c04f6-b8b6-11de-9846-1b058f2a8eb1

--- host/mnmTest/mnmTest.cpp
+++ host/mnmTest/mnmTest.cpp
@@ -13,6 +13,24 @@ MNMKit kit;
 MNMGlobal global;
 MNMSong song;
 
+void hexDump(uint8_t *data, uint16_t len) {
+  uint8_t cnt = 0;
+  for (int i = 0; i < len; i++) {
+    printf("%.2x ", data[i]);
+    cnt++;
+    if (cnt == 8) {
+      printf(" ");
+    }
+    if (cnt == 16) {
+      printf("\n");
+      cnt = 0;
+    }
+  }
+  if (cnt != 0) {
+    printf("\n");
+  }
+}
+
 static void usage() {
   fprintf(stderr, "Usage: mnmTest file.syx\n");
 }
@@ -20,17 +38,46 @@ static void usage() {
 uint8_t sysexBuf[8192];
 uint16_t sysexLen;
 
-void onGlobalMessageCallback() {
-  for (uint16_t i = 0; i < MidiSysex.recordLen; i++) {
-    printf("%x ", MidiSysex.data[i]);
+void parseSysex(uint8_t *data, uint16_t len) {
+  bool inSysex = false;
+  for (int i = 0; i < len; i++) {
+    uint8_t c = data[i];
+    
+    if (c == 0xF0) {
+      inSysex = true;
+      MidiSysex.reset();
+    } else {
+      if (inSysex) {
+	if (c == 0xF7) {
+	  inSysex = false;
+	  MidiSysex.end();
+	} else {
+	  MidiSysex.handleByte(c);
+	}
+      }
+    }
   }
-  printf("\n\n");
+}
+
+bool inCallback = false;
+void onGlobalMessageCallback() {
+  hexDump(MidiSysex.data, MidiSysex.recordLen);
       
   if (!global.fromSysex(MidiSysex.data, MidiSysex.recordLen)) {
     fprintf(stderr, "error parsing global\n");
   } else {
     printf("parsed global!\n");
   }
+  printf("\n");
+
+  if (!inCallback) {
+    inCallback = true;
+    uint16_t len = global.toSysex(sysexBuf, sizeof(sysexBuf));
+    hexDump(sysexBuf, len);
+    parseSysex(sysexBuf, len);
+  }
+
+  inCallback = false;
 }
 
 void onKitMessageCallback() {
