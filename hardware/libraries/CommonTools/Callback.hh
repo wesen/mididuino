c7e7e9bef93ba7d03aa01291049e25b502d72439
wesen3000 <wesen3000@6f8c04f6-b8b6-11de-9846-1b058f2a8eb1>
2009-07-14 17:33:14 +0000

    added callback handler class and object
    git-svn-id: https://mididuino.googlecode.com/svn/trunk@544 6f8c04f6-b8b6-11de-9846-1b058f2a8eb1

---
+++ hardware/libraries/CommonTools/Callback.hh
@@ -0,0 +1,44 @@
+#ifndef CALLBACK_H__
+#define CALLBACK_H__
+
+#include <inttypes.h>
+#include "Vector.hh"
+
+class Callback {
+public:
+  virtual void onCallback(uint16_t type, void *obj) = 0;
+};
+
+typedef void (*callback_func_t)(void *obj);
+template <uint8_t CLASSID, uint8_t CBID, int N> class CallbackHandler {
+public:
+  const static uint16_t id = ((uint16_t)CLASSID << 8) | CBID;
+
+  Vector<Callback *, N> callbacks;
+  Vector<callback_func_t, N> callbackFuncs;
+
+  CallbackHandler() {
+  }
+
+  void add(Callback *obj) {
+    callbacks.add(obj);
+  }
+
+  void remove(Callback *obj) {
+    callbacks.remove(obj);
+  }
+
+  void callback(void *data = NULL) {
+    for (int i = 0; i < callbacks.size; i++) {
+      if (callbacks.arr[i] != NULL) {
+	callbacks.arr[i]->onCallback(id, data);
+      }
+      if (callbackFuncs.arr[i] != NULL) {
+	callbackFuncs.arr[i](data);
+      }
+    }
+
+  }
+};
+
+#endif /* CALLBACK_H__ */
