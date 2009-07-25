3fa1d69f634e54a8f118878d2216b15eed655544
wesen3000 <wesen3000@6f8c04f6-b8b6-11de-9846-1b058f2a8eb1>
2009-07-25 13:14:13 +0000

    merge mididuino into main, poll gui once before setup()
    git-svn-id: https://mididuino.googlecode.com/svn/trunk@588 6f8c04f6-b8b6-11de-9846-1b058f2a8eb1

--- hardware/cores/minicommand2/main.cxx
+++ hardware/cores/minicommand2/main.cxx
@@ -1,7 +1,64 @@
 #include <Midi.h>
 
 #include "WProgram.h"
+extern "C" {
+#include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/wdt.h>
+#include <util/delay.h>
+#include <avr/pgmspace.h>
+#include <avr/eeprom.h>
+}
+
+void timer_init(void) {
+  TCCR0 = _BV(CS01);
+  //  TIMSK |= _BV(TOIE0);
+  
+  TCCR1A = _BV(WGM10); //  | _BV(COM1A1) | _BV(COM1B1); 
+  TCCR1B |= _BV(CS10) | _BV(WGM12); // every cycle
+#ifdef MIDIDUINO_MIDI_CLOCK
+  //  TIMSK |= _BV(TOIE1);
+#endif
+
+  TCCR2 = _BV(WGM20) | _BV(WGM21) | _BV(CS20) | _BV(CS21); // ) | _BV(CS21); // | _BV(COM21);
+  TIMSK |= _BV(TOIE2);
+}
+
+void init(void) {
+  /** Disable watchdog. **/
+  wdt_disable();
+  //  wdt_enable(WDTO_15MS);
+
+  /* move interrupts to bootloader section */
+  DDRC = 0xFF;
+  PORTC = 0x00;
+  MCUCR = _BV(IVCE);
+  MCUCR = _BV(SRE);
+
+  // activate lever converter
+  SET_BIT(DDRD, PD4);
+  SET_BIT(PORTD, PD4);
+
+  // activate background pwm
+  TCCR3B = _BV(WGM32) | _BV(CS30);
+  TCCR3A = _BV(WGM30) | _BV(COM3A1);
+  OCR3A = 160;
+
+  DDRE |= _BV(PE4) | _BV(PE5);
+  //  DDRB |= _BV(PB0);
+  //  DDRC |= _BV(PC3);
+
+  timer_init();
+}
+
+
+void (*jump_to_boot)(void) = (void(*)(void))0xF000;
+
+void start_bootloader(void) {
+  cli();
+  eeprom_write_word(START_MAIN_APP_ADDR, 0);
+  jump_to_boot();
+}
 
 void setup();
 void loop();
@@ -26,15 +83,17 @@ ISR(TIMER1_OVF_vect) {
 
 // XXX CMP to have better time
 
+static uint16_t oldsr = 0;
+
 void gui_poll() {
   static bool inGui = false;
-  if (inGui)
+  if (inGui) { 
     return;
-  else
+  } else {
     inGui = true;
+  }
   sei(); // reentrant interrupt
 
-  static uint16_t oldsr = 0;
   uint16_t sr = SR165.read16();
   if (sr != oldsr) {
     Buttons.clear();
@@ -98,9 +157,11 @@ int main(void) {
   clearLed();
   clearLed2();
 
-  for (int i = 0; i < GUI_NUM_BUTTONS; i++) {
-    SET_B_CURRENT(i);
-  }
+  uint16_t sr = SR165.read16();
+  Buttons.clear();
+  Buttons.poll(sr >> 8);
+  Encoders.poll(sr);
+  oldsr = sr;
 
   MidiSysex.addSysexListener(&MididuinoSysexListener);
 
