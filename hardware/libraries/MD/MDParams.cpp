0a85f1533334eb22369abf3feb97efc786b37407
wesen3000 <wesen3000@6f8c04f6-b8b6-11de-9846-1b058f2a8eb1>
2009-08-13 12:33:34 +0000

    added a few more models to MD
    git-svn-id: https://mididuino.googlecode.com/svn/trunk@669 6f8c04f6-b8b6-11de-9846-1b058f2a8eb1

--- hardware/libraries/MD/MDParams.cpp
+++ hardware/libraries/MD/MDParams.cpp
@@ -745,3 +745,118 @@ PGM_P model_param_name(uint8_t model, uint8_t param) {
     return get_param_name(get_model_param_names(model), param);
   }
 }
+
+static const uint8_t efm_rs_tuning[] PROGMEM = {
+   1,  3, 6, 9, 11, 14, 17, 19, 22, 25, 27, 30, 33, 35, 38, 41, 43,
+  46, 49, 51, 54, 57, 59, 62, 65, 67, 70, 73, 75, 78, 81, 83, 86,
+  89, 91, 94, 97, 99, 102, 105, 107, 110, 113, 115, 118, 121, 123,
+  126
+};
+static const uint8_t efm_hh_tuning[] PROGMEM = {
+  1, 5, 9, 14, 18, 22, 27, 31, 35, 39, 44, 48, 52, 56, 61, 65, 69,
+  73, 78, 82, 86, 91, 95, 99, 103, 108, 112, 116, 120, 125
+};
+static const uint8_t efm_cp_tuning[] PROGMEM = {
+  0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 29, 31, 33,
+  35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 62, 64, 66,
+  68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 88, 90, 92, 94, 95, 97, 99, 101,
+  103, 105, 107, 109, 111, 113, 115, 117, 119, 121, 123, 125, 127};
+
+static const uint8_t efm_sd_tuning[] PROGMEM = {
+  1, 5, 9, 14, 18, 22, 27, 31, 35, 39, 44, 48, 52, 56, 61, 65, 69, 73, 78, 82, 
+86, 91, 95, 99, 103, 108, 112, 116, 120, 125, 
+};
+
+static const uint8_t efm_xt_tuning[] PROGMEM = {
+  1, 7, 12, 17, 23, 28, 33, 39, 44, 49, 55, 60, 65, 71, 76, 81, 87, 92, 97, 102, 
+  108, 113, 118, 124, 
+};
+
+static const uint8_t efm_bd_tuning[] PROGMEM = {
+  1, 3, 6, 9, 11, 14, 17, 19, 22, 25, 27, 30, 33, 35, 38, 41, 43, 46, 49, 51, 54, 
+  57, 59, 62, 65, 67, 70, 73, 75, 78, 81, 83, 86, 89, 91, 94, 97, 99, 102, 105, 107, 
+  110, 113, 115, 118, 121, 123, 126, 
+};
+static const uint8_t trx_cl_tuning[] PROGMEM = {
+  5, 11, 17, 23, 29, 36, 42, 48, 54, 60, 66, 72, 78, 84, 91, 97, 103, 109, 115, 121, 
+  127, 
+};
+static const uint8_t trx_sd_tuning[] PROGMEM = {
+  3, 13, 24, 35, 45, 56, 67, 77, 88, 98, 109, 120, 
+};
+static const uint8_t trx_xc_tuning[] PROGMEM = {
+  1, 6, 11, 17, 22, 27, 33, 38, 43, 49, 54, 60, 65, 70, 76, 81, 86, 92, 97, 102, 
+  108, 113, 118, 124, 
+};
+static const uint8_t trx_xt_tuning[] PROGMEM = {
+  2, 7, 12, 18, 23, 28, 34, 39, 44, 49, 55, 60, 65, 71, 76, 81, 87, 92, 97, 103, 
+  108, 113, 118, 124,
+};
+static const uint8_t trx_bd_tuning[] PROGMEM = {
+  1, 7, 12, 17, 23, 28, 33, 39, 44, 49, 55, 60, 66, 71, 76, 82, 87, 92, 98, 103, 
+  108, 114, 119, 124, 
+};
+static const uint8_t rom_tuning[] PROGMEM = {
+  0, 2, 5, 7, 9, 12, 14, 16, 19, 21, 23, 26, 28, 31, 34, 37, 40, 43, 46, 49, 52, 
+  55, 58, 61, 64, 67, 70, 73, 76, 79, 82, 85, 88, 91, 94, 97, 100, 102, 105, 107, 
+  109, (112), 114, 116, 119, 121, 123, 125, 
+};
+
+static const uint8_t gnd_sn_tuning[] PROGMEM = {
+  0, 2, 3, 4, 6, 7, 8, 10, 11, 12, 14, 15, 16, 18, 19, 20,
+  22, 23, 24, 26, 27, 28, 30, 31, 32, 34, 35, 36, 38, 39, 40, 42, 43, 44, 46, 47, 48,
+  50, 51, 52, 54, 55, 56, 58, 59, 60, 62, 63, 64, 66, 67, 68, 70, 71, 72, 74, 75, 76,
+  78, 79, 80, 82, 83, 84, 86, 87, 88, 90, 91, 92, 94, 95, 96, 98, 99, 100,
+  102, 103, 104, 106, 107, 108, 110, 111, 112, 114, 115, 116, 118, 119, 120,
+  122, 123, 124, 126, 127
+};
+
+static const uint8_t trx_b2_tuning[] PROGMEM = {
+  31, 33, 36, 39, 43, 45, 48, 51, 56, 60, 62, 66, 70, 74, 79, 83, 89, 94, 97, 104, 108, 113, 120, 125
+};
+
+static const uint8_t trx_rs_tuning[] PROGMEM = {
+  2, 10, 17, 26, 36, 45, 55, 66, 78, 90, 103, 117
+};
+
+static const uint8_t efm_cb_tuning[] PROGMEM = {
+  2, 6, 10, 14, 19, 23, 27, 32, 36, 40, 44, 48, 53, 57, 62, 66, 70, 74, 78, 83,
+  87, 91, 96, 100, 104, 108, 113, 117, 121, 126
+};
+
+static const tuning_t rom_tuning_t = { ROM_MODEL,    45, 
+				       sizeof(rom_tuning), 4,   rom_tuning };
+static const tuning_t tunings[] = {
+  { EFM_RS_MODEL, 59, sizeof(efm_rs_tuning), 4, efm_rs_tuning },
+  { EFM_HH_MODEL, 59, sizeof(efm_hh_tuning), 8, efm_hh_tuning },
+  { EFM_CP_MODEL, 47, sizeof(efm_cp_tuning), 3, efm_cp_tuning },
+  { EFM_SD_MODEL, 47, sizeof(efm_sd_tuning), 5, efm_sd_tuning },
+  { EFM_XT_MODEL, 29, sizeof(efm_xt_tuning), 7, efm_xt_tuning },
+  { EFM_BD_MODEL, 20, sizeof(efm_bd_tuning), 4, efm_bd_tuning },
+  { TRX_CL_MODEL, 83, sizeof(trx_cl_tuning), 7, trx_cl_tuning },
+  { TRX_SD_MODEL, 53, sizeof(trx_sd_tuning), 12, trx_sd_tuning },
+  { TRX_XC_MODEL, 41, sizeof(trx_xc_tuning), 6, trx_xc_tuning },
+  { TRX_XT_MODEL, 38, sizeof(trx_xt_tuning), 6, trx_xt_tuning },
+  { TRX_BD_MODEL, 23, sizeof(trx_bd_tuning), 7, trx_bd_tuning },
+  { GND_SN_MODEL, 29, sizeof(gnd_sn_tuning), 3, gnd_sn_tuning },
+  { TRX_B2_MODEL, 21, sizeof(trx_b2_tuning), 8, trx_b2_tuning },
+  { TRX_RS_MODEL, 53, sizeof(trx_rs_tuning), 13, trx_rs_tuning },
+  { EFM_CB_MODEL, 41, sizeof(efm_cb_tuning), 5, efm_cb_tuning },
+  { ROM_MODEL,    45, sizeof(rom_tuning),    4, rom_tuning    },
+};
+
+tuning_t const *track_tunings[16];
+
+const tuning_t PROGMEM *MDClass::getModelTuning(uint8_t model) {
+  uint8_t i;
+  if (((model >= 128) && (model <= 159))) {
+    return &rom_tuning_t;
+  }
+  for (i = 0; i < countof(tunings); i++) {
+    if (model == tunings[i].model) {
+      return tunings + i;
+    }
+  }
+
+  return NULL;
+}
