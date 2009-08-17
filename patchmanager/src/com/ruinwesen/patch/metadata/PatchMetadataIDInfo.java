3fc9375d7d4241774b233557fe8a46a0aa52de3d
chresan <chresan@6f8c04f6-b8b6-11de-9846-1b058f2a8eb1>
2009-08-17 22:06:24 +0000

    removed unused import
    git-svn-id: https://mididuino.googlecode.com/svn/trunk@782 6f8c04f6-b8b6-11de-9846-1b058f2a8eb1

--- patchmanager/src/com/ruinwesen/patch/metadata/PatchMetadataIDInfo.java
+++ patchmanager/src/com/ruinwesen/patch/metadata/PatchMetadataIDInfo.java
@@ -41,7 +41,6 @@ import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.log4j.lf5.viewer.LogFactor5Dialog;
 
 public final class PatchMetadataIDInfo {
 
