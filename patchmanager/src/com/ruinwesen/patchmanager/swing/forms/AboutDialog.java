e2c7f12c245ff87eca6421cedb760d81c7bfb66d
chresan <chresan@6f8c04f6-b8b6-11de-9846-1b058f2a8eb1>
2009-08-17 19:47:49 +0000

    show r&w logo in about dialog
    git-svn-id: https://mididuino.googlecode.com/svn/trunk@761 6f8c04f6-b8b6-11de-9846-1b058f2a8eb1

--- patchmanager/src/com/ruinwesen/patchmanager/swing/forms/AboutDialog.java
+++ patchmanager/src/com/ruinwesen/patchmanager/swing/forms/AboutDialog.java
@@ -29,23 +29,27 @@
 package com.ruinwesen.patchmanager.swing.forms;
 
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Color;
+import java.awt.event.MouseEvent;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
-import javax.swing.JButton;
+import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
 
-import name.cs.csutils.CSAction;
+import name.cs.csutils.CSEventAdapter;
+import name.cs.csutils.IconLoader;
 
 import com.ruinwesen.patchmanager.swing.SwingPatchManager;
 
 public class AboutDialog {
 
     private SwingPatchManager patchmanager;
+    
+    
     
     public AboutDialog(SwingPatchManager patchmanager) {
         this.patchmanager = patchmanager;
@@ -55,34 +59,74 @@ public class AboutDialog {
         new AboutDialog(patchmanager).showDialog();
     }
 
+    
+    
     private void showDialog() {
+        
+        JPanel iconPanel = new JPanel();
+        iconPanel.setBackground(new Color(0x171717));
+        iconPanel.setLayout(new BorderLayout());
+        iconPanel.add(newLabel(IconLoader.getInstance().getIcon("rw-logo")),
+                BorderLayout.NORTH);
+        iconPanel.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
+        
+        
+        
         JPanel panel = new JPanel();
+        panel.setBackground(new Color(0x171717));
+        panel.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-        panel.add(new JLabel("PatchManager"));
-        panel.add(new JLabel("Version: "+patchmanager.getVersion()));
-        panel.add(new JLabel("Build: "+patchmanager.getBuildNumber()+
+        panel.add(newLabel("PatchManager"));
+        panel.add(newLabel("(C) 2009 Ruin & Wesen"));
+        panel.add(Box.createVerticalStrut(5));
+        panel.add(newLabel("Version: "+patchmanager.getVersion()));
+        panel.add(newLabel("Build: "+patchmanager.getBuildNumber()+
                 " Time: "+patchmanager.getBuildTimestamp()));
+        panel.add(Box.createVerticalStrut(5));
+        panel.add(newLabel("Credits"));
+        panel.add(newLabel("- Manuel Odendahl"));
+        panel.add(newLabel("- Christian Schneider"));
         
         final JDialog dialog = new JDialog(patchmanager.getFrame(),"About Patchmanager");
         
-        JButton btn = 
-        new JButton(new CSAction("Close")
-        .useActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    dialog.setVisible(false);
-                }
-        }
-        ));
-        
-        btn.setHorizontalAlignment(SwingConstants.CENTER);
-        panel.add(btn);
+        dialog.getContentPane().addMouseListener(new CSEventAdapter(){
+            public void mouseClicked(MouseEvent e) {
+                close(dialog);
+            }
+        });
 
         dialog.getContentPane().setLayout(new BorderLayout());
+        dialog.getContentPane().add(iconPanel,BorderLayout.WEST);
         dialog.getContentPane().add(panel,BorderLayout.CENTER);
+        dialog.setResizable(false);
         dialog.setModal(true);
         dialog.pack();
         dialog.setLocationByPlatform(true);
         dialog.setVisible(true);
+    }
+
+
+    private void close(JDialog dialog) {
+        dialog.setVisible(false);
+        dialog.dispose();
+    }
+    
+    private JLabel newLabel(String text) {
+        JLabel label = newLabel();
+        label.setText(text);
+        return label;
+    }
+
+    private JLabel newLabel(ImageIcon icon) {
+        JLabel label = newLabel();
+        label.setIcon(icon);
+        return label;
+    }
+
+    private JLabel newLabel() {
+        JLabel label = new JLabel();
+        label.setForeground(Color.WHITE);
+        return label;
     }
     
 }
