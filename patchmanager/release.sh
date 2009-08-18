91e482decdbef85e4cf14d2b1676e7fac067c339
wesen3000 <wesen3000@6f8c04f6-b8b6-11de-9846-1b058f2a8eb1>
2009-08-18 12:55:55 +0000

    added new build scripts
    git-svn-id: https://mididuino.googlecode.com/svn/trunk@786 6f8c04f6-b8b6-11de-9846-1b058f2a8eb1

---
+++ patchmanager/release.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#ant release
+rm *zip *.tar.gz
+cd dist
+zip -r ../patchmanager-win.zip * -x \*.svn\*
+tar zcvf ../patchmanager-linux.tar.gz --exclude \*.exe *
+cd ../osx-dist
+zip -r ../patchmanager-macosx.zip README.txt PatchManager.app -x \*PatchManagerAdmin\*
+cd ..
