97256166f34fe1ba2a408a61c1bca518eb856d27
wesen3000 <wesen3000@6f8c04f6-b8b6-11de-9846-1b058f2a8eb1>
2009-08-19 08:23:59 +0000

    added build instructions for java5 version
    git-svn-id: https://mididuino.googlecode.com/svn/trunk@811 6f8c04f6-b8b6-11de-9846-1b058f2a8eb1

--- patchmanager/release.sh
+++ patchmanager/release.sh
@@ -6,5 +6,5 @@ cd dist
 zip -r ../patchmanager-win.zip * -x \*.svn\*
 tar zcvf ../patchmanager-linux.tar.gz --exclude \*.exe *
 cd ../osx-dist
-zip -r ../patchmanager-macosx.zip README.txt PatchManager.app -x \*PatchManagerAdmin\*
+zip -r ../patchmanager-macosx.zip README.txt PatchManager.app PatchManagerJava5.app -x \*PatchManagerAdmin\* 
 cd ..
