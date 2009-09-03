a75f982e48ab47df096065fb45d3c82a746d2a8a
wesen3000 <wesen3000@6f8c04f6-b8b6-11de-9846-1b058f2a8eb1>
2009-09-03 17:24:54 +0000

    added empty functions to get last notes from note handler
    git-svn-id: https://mididuino.googlecode.com/svn/trunk@878 6f8c04f6-b8b6-11de-9846-1b058f2a8eb1

--- hardware/libraries/MidiTools/NoteHandler.cpp
+++ hardware/libraries/MidiTools/NoteHandler.cpp
@@ -11,3 +11,11 @@ void NoteHandler::onNoteOnCallback(uint8_t *msg) {
 
 void NoteHandler::onNoteOffCallback(uint8_t *msg) {
 }
+
+uint8_t getLastPressedNotes(uint8_t *pitches, uint8_t *velocity, uint8_t maxNotes) {
+  return 0;
+}
+
+uint8_t getLastPressedNotesOrderPitch(uint8_t *pitches, uint8_t *velocity, uint8_t maxNotes) {
+  return 0;
+}
