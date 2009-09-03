14973c6922da7a45532b165c0eafd867285ac608
wesen3000 <wesen3000@6f8c04f6-b8b6-11de-9846-1b058f2a8eb1>
2009-09-03 17:32:53 +0000

    Added new test for playing a single note
    git-svn-id: https://mididuino.googlecode.com/svn/trunk@885 6f8c04f6-b8b6-11de-9846-1b058f2a8eb1

--- host/Test/noteHandlerTest.cpp
+++ host/Test/noteHandlerTest.cpp
@@ -7,6 +7,10 @@
 
 struct NoteHandlerFixture {
   NoteHandlerFixture() {
+    for (int i = 0; i < countof(pitches); i++) {
+      pitches[i] = 255;
+      velocities[i] = 255;
+    }
   }
 
   NoteHandler noteHandler;
@@ -17,4 +21,13 @@ struct NoteHandlerFixture {
 TEST_F (NoteHandlerFixture, EmptyNoteHandler) {
   uint8_t num = noteHandler.getLastPressedNotes(pitches, velocities, countof(pitches));
   CHECK_EQUAL(0, num);
+}
+
+TEST_F (NoteHandlerFixture, OneNoteOnHandler) {
+  uint8_t note[3] = { MIDI_NOTE_ON, 100, 100 };
+  noteHandler.onNoteOnCallback(note);
+  uint8_t num = noteHandler.getLastPressedNotes(pitches, velocities, countof(pitches));
+  CHECK_EQUAL(1, num);
+  CHECK_EQUAL(100, pitches[0]);
+  CHECK_EQUAL(100, velocities[0]);
 }
