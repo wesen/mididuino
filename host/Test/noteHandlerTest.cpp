527d4dd68a797cc8f9431cbd0daeda5c046c5887
wesen3000 <wesen3000@6f8c04f6-b8b6-11de-9846-1b058f2a8eb1>
2009-09-03 17:25:22 +0000

    added first test for notehandler
    git-svn-id: https://mididuino.googlecode.com/svn/trunk@882 6f8c04f6-b8b6-11de-9846-1b058f2a8eb1

--- host/Test/noteHandlerTest.cpp
+++ host/Test/noteHandlerTest.cpp
@@ -5,3 +5,16 @@
 #include "WProgram.h"
 #include <helpers.h>
 
+struct NoteHandlerFixture {
+  NoteHandlerFixture() {
+  }
+
+  NoteHandler noteHandler;
+  uint8_t pitches[8];
+  uint8_t velocities[8];
+};
+
+TEST_F (NoteHandlerFixture, EmptyNoteHandler) {
+  uint8_t num = noteHandler.getLastPressedNotes(pitches, velocities, countof(pitches));
+  CHECK_EQUAL(0, num);
+}
