816492bfe7d4997f48f8d786ad125477c92a1512
wesen3000 <wesen3000@6f8c04f6-b8b6-11de-9846-1b058f2a8eb1>
2009-10-15 23:59:49 +0000

    compile cppunitlite on windows, print checks as ints (workaround for unsigned char)
    git-svn-id: https://mididuino.googlecode.com/svn/trunk@1000 6f8c04f6-b8b6-11de-9846-1b058f2a8eb1

--- thirdparty/CppUnitLite2/src/ExceptionHandler.cpp
+++ thirdparty/CppUnitLite2/src/ExceptionHandler.cpp
@@ -1,3 +1,6 @@
+#include <string.h>
+#include <stdio.h>
+
 #include "ExceptionHandler.h"
 #include "TestResult.h"
 #include "Failure.h"
--- thirdparty/CppUnitLite2/src/TestMacros.h
+++ thirdparty/CppUnitLite2/src/TestMacros.h
@@ -19,14 +19,14 @@ void CheckEqual( TestResult& result_, const Type1& expected, const Type2& actual
     try {                                                                                              
         if (!(expected == actual)) {                                                               
             std::stringstream msg;                                                                     
-            msg << "expected: '" << (expected) << "' but was: '" << (actual) << "'" << std::ends;   
+            msg << "expected: '" << (expected) << "' but was off: '" << (int)(actual) << "'" << std::ends;   
             result_.AddFailure (Failure (msg.str().c_str(), name, file, line));                   
         }                                                                                              
     } catch( const TestException& e ) {                                                                
         UnitExceptionHandler::Handle(result_, e, name, file, line );                             
     } catch(...) {                                                                                     
         std::stringstream msg;                                                                         
-        msg << "expected: '" << (expected) << "' but was: '" << (actual) << "'" << std::ends;       
+        msg << "expected: '" << (expected) << "' but was off: '" << (int)(actual) << "'" << std::ends;       
         UnitExceptionHandler::Handle(result_, msg.str().c_str(), name, file, line);                   
     }                                                                                                  
 }
@@ -38,7 +38,7 @@ void CheckClose( TestResult& result_, const Type1& expected, const Type2& actual
         if ((((expected)-(actual)) > (epsilon)) ||                                                    
             (((actual)-(expected)) > (epsilon))) {                                                    
             std::stringstream msg;                                                                     
-            msg  << "expected: " << (expected) << " but was: " << (actual) << std::ends;            
+            msg  << "expected: " << (expected) << " but was off2: " << (actual) << std::ends;            
             result_.AddFailure (Failure (msg.str().c_str(),  name, file, line));                
         }                                                                                              
     } catch( const TestException& e ) {                                                                
