810cf0f81aed7d77056d7659306805b46242b4ad
wesen3000 <wesen3000@6f8c04f6-b8b6-11de-9846-1b058f2a8eb1>
2009-11-16 19:19:08 +0000

    added weak functions to core
    git-svn-id: https://mididuino.googlecode.com/svn/trunk@1078 6f8c04f6-b8b6-11de-9846-1b058f2a8eb1

---
+++ hardware/cores/minicommand2/weak_funcs.cpp
@@ -0,0 +1,25 @@
+#include <GUI.h>
+
+void __attribute__((weak)) setup() {
+}
+
+void __attribute__((weak)) loop() {
+}
+
+bool __attribute__((weak)) onNoteOn(uint8_t *msg) {
+}
+
+bool __attribute__((weak)) onNoteOff(uint8_t *msg) {
+}
+
+bool __attribute__((weak)) onControlChange(uint8_t *msg) {
+}
+
+bool __attribute__((weak)) on16Callback(uint32_t pos) {
+}
+
+bool __attribute__((weak)) on32Callback(uint32_t pos) {
+}
+
+bool __attribute__((weak)) eventHandler(gui_event_t *evt) {
+}
