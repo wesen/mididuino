30140539c464a89ab022b143e7ad7b5955b24fd1
Manuel Odendahl <wesen@ruinwesen.com>
2009-11-28 12:16:53 +0100

    make standalone version of the updater

---
+++ patchmanager/src/com/ruinwesen/patchmanager/swing/wizards/FaderfoxMidiSendWizard.java
@@ -0,0 +1,25 @@
+package com.ruinwesen.patchmanager.swing.wizards;
+
+import javax.swing.JFrame;
+
+import com.ruinwesen.patchmanager.swing.form.Wizard;
+import com.ruinwesen.patchmanager.swing.forms2.MidiSendForm;
+
+public class FaderfoxMidiSendWizard extends MidiSendWizard {
+
+	public FaderfoxMidiSendWizard() {
+		super();
+		setTitle("Faderfox Firmware Update");
+		midisendForm.getMidiSend().setDeviceID(0x50); // faderfox adapter
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+        Wizard w = new FaderfoxMidiSendWizard();
+        w.showDialog((JFrame)null);
+
+	}
+
+}
