cfe12167ff6d505006e400f9155a07959ffc01d7
Manuel Odendahl <wesen@ruinwesen.com>
2010-05-24 18:42:07 +0200

    removed patchmanager

--- patchmanager/patchcli.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-BASE_DIR=/Users/manuel/code/mididuino-core/patchmanager/dist/patchmanager
-CLASSPATH=$BASE_DIR/lib:$BASE_DIR/patchmanager.jar
-
-java -cp "$CLASSPATH" com.ruinwesen.patchmanager.cli.CLIUtil "$@"
