e7e463190d5a089dc3aad6b495332a7fd05b9732
Manuel Odendahl <wesen@ruinwesen.com>
2010-07-08 14:41:10 -0700

    updated build process for linux

--- build/linux/dist.sh
+++ build/linux/dist.sh
@@ -11,24 +11,24 @@ echo Creating linux distribution for revision $REVISION...
 rm -rf work/classes
 
 # remove any old boogers
-rm -rf arduino
-rm -rf arduino-*
+rm -rf midi-ctrl
+rm -rf midi-ctrl-*
 
-mv work arduino
+mv work midi-ctrl
 
 # remove boogers
-find arduino -name "*~" -exec rm -f {} ';'
-find arduino -name ".DS_Store" -exec rm -f {} ';'
-find arduino -name "._*" -exec rm -f {} ';'
-find arduino -name "Thumbs.db" -exec rm -f {} ';'
+find midi-ctrl -name "*~" -exec rm -f {} ';'
+find midi-ctrl -name ".DS_Store" -exec rm -f {} ';'
+find midi-ctrl -name "._*" -exec rm -f {} ';'
+find midi-ctrl -name "Thumbs.db" -exec rm -f {} ';'
 
 # clean out the cvs entries
-find arduino -name ".svn" -exec rm -rf {} ';' 2> /dev/null
+find midi-ctrl -name ".svn" -exec rm -rf {} ';' 2> /dev/null
 
 # zip it all up for release
 echo Creating tarball and finishing...
-P5=arduino-$REVISION
-mv arduino $P5
+P5=midi-ctrl-$REVISION
+mv midi-ctrl $P5
 
 tar cfz $P5.tgz $P5
 # nah, keep the new directory around
--- build/linux/make.sh
+++ build/linux/make.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
-# Part of the Arduino project
-# http://arduino.berlios.de
-#
-# this is derived from the processing project
-# http://www.processing.org
-#
 # This file is subjected to the GPL License
 
 # NOTE: before running this script, you must set CLASSPATH to
 # your standard Java classpath, and DIRAVR to the root of your
 # avr-gcc installation.
 
+DISTNAME=MidiCtrl
+HARDWAREDIR=../../hardware
+DISTDIR=../../../mididuino-dist
+SHAREDDIST=${DISTDIR}/shared
+RESOURCES=`pwd`/work
 
 ### -- SETUP WORK DIR -------------------------------------------
 
@@ -28,52 +27,105 @@ else
   mkdir -p work/classes/processing/app/tools
   mkdir -p work/lib/build
 
-  echo Unzipping reference...
-  unzip -q -d work ../shared/reference.zip
+	cp ../../readme.txt work
+	cp ../../license.txt work
+	cp ../../building.txt work
 
-  cp dist/arduino work/
+	cp -r ../../app/lib "$RESOURCES/"
+
+  cp dist/midi-ctrl work/
 fi
 
+# Build midi-send tool
 cd ../../hardware/tools/mididuino
 make -f Makefile.linux
 cd ../../../build/linux
 
 echo Copying shared and core files...
-cp -r ../shared/* work
+cp -r ${SHAREDDIST}/* work
 rm -rf work/dist
-cp -r ../../hardware work
-cp ../../hardware/tools/mididuino/midi-send work/hardware/tools
-cp ../../hardware/tools/avr-extras/avr5.x work/hardware/avr/avr/lib/ldscripts
-rm work/reference.zip
+cp -r ../../hardware/tools "$RESOURCES/"
+# cp ../../hardware/tools/avr-extras/avr5.x "$RESOURCES/hardware/avr/avr/lib/ldscripts"
+
+cp -r "$HARDWAREDIR" "$RESOURCES/"
+cp ../../app/lib/antlr.jar "$RESOURCES/lib"
+cp ../../app/lib/jna.jar "$RESOURCES/lib"
+cp ../../app/lib/oro.jar "$RESOURCES/lib"
 
 echo Copying examples...
-cp -r ../shared/dist/examples work/
-
-echo Copying dist files...
-cp -r dist/lib work/
-cp -r dist/tools work/hardware/
+mkdir "$RESOURCES/examples"
+cp -r ../../minicommand-sketches/PublishedFirmwares "$RESOURCES/examples/"
+cp -r ../../minicommand-sketches/Tutorials "$RESOURCES/examples/"
 
 ### -- START BUILDING -------------------------------------------
 
 # move to root 'arduino' directory
 cd ../..
 
-### -- BUILD PDE ------------------------------------------------
+### -- BUILD CORE ------------------------------------------------
 
-cd app
+echo Building processing.core...
+
+cd core
+
+#CLASSPATH=/System/Library/Frameworks/JavaVM.framework/Classes/classes.jar:/System/Library/Frameworks/JavaVM.framework/Classes/ui.jar:/System/Library/Java/Extensions/QTJava.zip
+#export CLASSPATH
+
+perl preproc.pl
+
+mkdir -p bin
+javac -source 1.5 -target 1.5 -d bin \
+  src/processing/core/*.java \
+  src/processing/xml/*.java
+
+rm -f "$RESOURCES/lib/core.jar"
+
+cd bin && \
+		zip -rq "$RESOURCES/lib/core.jar" \
+  processing/core/*.class \
+  processing/xml/*.class
+cd ..
+
+# head back to "processing/app"
+echo `pwd`
+cd ../app 
+echo `pwd`
+
+
+
+### -- BUILD PDE ------------------------------------------------
 
 echo Building the PDE...
 
-# compile the code as java 1.3, so that the application will run and
-# show the user an error, rather than crapping out with some strange
-# "class not found" crap
-#jikes -classpath ../build/linux/work/classes:../build/linux/work/lib/antlr.jar:../build/linux/work/lib/oro.jar:../build/linux/work/lib/registry.jar:../build/linux/work/lib/RXTXcomm.jar:../build/linux/work/lib/mrj.jar:$CLASSPATH -d ../build/linux/work/classes tools/*.java preproc/*.java syntax/*.java *.java 
-javac -source 1.4 -target 1.4 -classpath ../build/linux/work/class:../build/linux/work/lib/antlr.jar:../build/linux/work/lib/oro.jar:../build/linux/work/lib/registry.jar:../build/linux/work/lib/RXTXcomm.jar:../build/linux/work/lib/mrj.jar:$CLASSPATH -d ../build/linux/work/classes ../core/*.java tools/*.java preproc/*.java syntax/*java *.java
+# For some reason, javac really wants this folder to exist beforehand.
+rm -rf ../build/linux/work/classes
+mkdir ../build/linux/work/classes
+# Intentionally keeping this separate from the 'bin' folder
+# used by eclipse so that they don't cause conflicts.
+
+javac \
+    -Xlint:deprecation \
+    -source 1.5 -target 1.5 \
+    -classpath "/usr/lib/jvm/java-6-openjdk/lib/tools.jar:$RESOURCES/lib/core.jar:$RESOURCES/lib/antlr.jar:$RESOURCES/lib/ecj.jar:$RESOURCES/lib/jna.jar:$RESOURCES/lib/oro.jar" \
+    -d ../build/linux/work/classes \
+    src/processing/app/*.java \
+    src/processing/app/debug/*.java \
+    src/processing/app/linux/*.java \
+    src/processing/app/preproc/*.java \
+    src/processing/app/syntax/*.java \
+    src/processing/app/tools/*.java \
+    src/processing/app/library/*.java
 
 cd ../build/linux/work/classes
-rm -f ../lib/pde.jar
-zip -0rq ../lib/pde.jar .
+rm -f "$RESOURCES/lib/pde.jar"
+zip -0rq "$RESOURCES/lib/pde.jar" .
 cd ../..
+
+rm -rf "$RESOURCES/classes"
+# get updated core.jar and pde.jar; also antlr.jar and others
+#mkdir -p work/${DISTNAME}.app/Contents/Resources/Java/
+#cp work/lib/*.jar work/${DISTNAME}.app/Contents/Resources/Java/
+
 
 echo
 echo Done.
