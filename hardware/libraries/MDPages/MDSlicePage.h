6ea72eefc68c085311b743edb9c24f16a37bf093
Manuel Odendahl <wesen@ruinwesen.com>
2011-01-16 22:59:29 +0100

    Added skeleton for new slice page

---
+++ hardware/libraries/MDPages/MDSlicePage.h
@@ -0,0 +1,37 @@
+/* Copyright (c) 2009 - http://ruinwesen.com/ */
+
+#ifndef MD_SLICE_PAGE_H__
+#define MD_SLICE_PAGE_H__
+
+#include "WProgram.h"
+
+/**
+ * \addtogroup MD Elektron MachineDrum
+ *
+ * @{
+ *
+ * \addtogroup md_pages MachineDrum Pages
+ *
+ * @{
+ **/
+
+/**
+ * \addtogroup md_slice_page MachineDrum Slice Pattern Page
+ *
+ * @{
+ *
+ **/
+
+/**
+ * This page is used to slice tracks and reverse them.
+ **/
+
+class MDStudioSketch;
+
+class MDSlicePage: public EncoderPage {
+};
+
+
+/* @} @} @} */
+
+#endif /* XXX_H__ */
