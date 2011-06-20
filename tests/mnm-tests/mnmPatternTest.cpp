33a52273ff52344d7989b095c610823c75494729
Manuel Odendahl <wesen@ruinwesen.com>
2011-06-20 22:12:59 +0200

    some more sysex comparison tests

--- tests/mnm-tests/mnmPatternTest.cpp
+++ tests/mnm-tests/mnmPatternTest.cpp
@@ -4,10 +4,9 @@
 #include <WProgram.h>
 #include <MNM.h>
 
-bool compareMonoSysex(uint8_t *buf, uint8_t *buf2) {
+bool compareMonoSysex(uint8_t *buf, uint8_t *buf2, uint16_t len) {
   uint8_t buftmp[8192];
   uint8_t buftmp2[8192];
-  uint16_t len = 0x1978;
   MNMSysexDecoder decoder(DATA_ENCODER_INIT(buf + 9, len - 11));
   decoder.get(buftmp + 1, len - 10);
   MNMSysexDecoder decoder2(DATA_ENCODER_INIT(buf2 + 10, len - 10));
@@ -17,22 +16,61 @@ bool compareMonoSysex(uint8_t *buf, uint8_t *buf2) {
       printf("mono sysex different at 0x%x, %x != %x\n", i + 10, buftmp[i], buftmp2[i]);
       return false;
     } else {
-      printf("%x: %x == %x\n", i + 10, buftmp[i], buftmp2[i]);
+      //      printf("%x: %x == %x\n", i + 10, buftmp[i], buftmp2[i]);
     }
   }
   return true;
 }
 
-
 class MNMHandler : public MNMCallback {
 public:
+  void onMNMKit() {
+    MNMKit kit;
+    printf("received\n");
+    hexdump(MidiSysex.data, MidiSysex.recordLen);
+
+    if (kit.fromSysex(MidiSysex.data + 5, MidiSysex.recordLen - 5)) {
+      printf("parsed mnm kit sysex\n");
+      kit.print();
+      uint8_t buf[8192];
+      uint16_t retlen = kit.toSysex(buf, sizeof(buf));
+      if (compareMonoSysex(MidiSysex.data, buf, 0x2ba)) {
+        printf("Buffer comparison successful!\n");
+      }
+      retlen = 0;
+      //      hexdump(buf+1, retlen - 2);
+    } else {
+      printf("error parsing mnm kit message\n");
+    }
+  }
+
+  void onMNMGlobal() {
+    MNMGlobal global;
+    printf("received\n");
+    hexdump(MidiSysex.data, MidiSysex.recordLen);
+
+    if (global.fromSysex(MidiSysex.data + 5, MidiSysex.recordLen - 5)) {
+      printf("parsed mnm global sysex\n");
+      global.print();
+      uint8_t buf[8192];
+      uint16_t retlen = global.toSysex(buf, sizeof(buf));
+      if (compareMonoSysex(MidiSysex.data, buf, 0x110)) {
+        printf("Buffer comparison successful!\n");
+      }
+      hexdump(buf+1, retlen - 2);
+    } else {
+      printf("error parsing mnm kit message\n");
+    }
+    MNM.requestPattern(0);
+  }
+  
   void onMNMPattern() {
     MNMPattern pattern;
     printf("received\n");
     hexdump(MidiSysex.data, MidiSysex.recordLen);
 		
     if (pattern.fromSysex(MidiSysex.data + 5, MidiSysex.recordLen - 5)) {
-#if 1
+#if 0
       printf("parsed mnm pattern sysex\n");
       pattern.print();
       pattern.setAllTrig(2, 0);
@@ -45,14 +83,14 @@ public:
       pattern.print();
 #endif
 
-      printf("chordNotes[191] %p\n", &pattern.chordNotes[191]);
       uint8_t buf[8192];
-      printf("buf %p chordNotes[191] %p\n", buf, &pattern.chordNotes[191]);
       uint16_t retlen = pattern.toSysex(buf, sizeof(buf));
-      compareMonoSysex(MidiSysex.data, buf);
+      if (compareMonoSysex(MidiSysex.data, buf, 0x1978)) {
+        printf("Buffer comparison successful!\n");
+        //        MidiUart.sendRaw(buf, retlen);
+      }
       printf("retlen: %d\n", retlen);
       hexdump(buf + 1, retlen - 2);
-      MidiUart.sendRaw(buf, retlen);
 #if 0
       if (pattern.fromSysex(buf + 6, retlen - 7)) {
         printf("reimported\n");
@@ -65,6 +103,8 @@ public:
     } else {
       printf("error parsing mnm sysex message\n");
     }
+
+    MNM.requestKit(2);
   }
 };
 
@@ -73,9 +113,12 @@ int main(void) {
 
   MNMHandler mnmHandler;
   MNMSysexListener.addOnPatternMessageCallback(&mnmHandler, (mnm_callback_ptr_t)&MNMHandler::onMNMPattern);
+  MNMSysexListener.addOnKitMessageCallback(&mnmHandler, (mnm_callback_ptr_t)&MNMHandler::onMNMKit);
+  MNMSysexListener.addOnGlobalMessageCallback(&mnmHandler, (mnm_callback_ptr_t)&MNMHandler::onMNMGlobal);
 
-  MidiUart.init(0, 0);
-  MNM.requestPattern(0);
+  //  MidiUart.init("Elektron TM-1", "Elektron TM-1");
+  MidiUart.init("Port 1 - Generic", "Port 1 - Generic");
+  MNM.requestGlobal(0);
 
   for (;;) {
     MidiUart.runLoop();
