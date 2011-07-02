cbed3174b0cacbe7abe4ff52d9fd051f3508ef44
Manuel Odendahl <wesen@ruinwesen.com>
2011-07-02 18:51:25 +0200

    Adapt existing host apps to new makefile schema

--- host-apps/profiler/merger.cpp
+++ host-apps/profiler/merger.cpp
@@ -9,35 +9,13 @@
 
 #include <stdio.h>
 
-MidiUartOSXClass MidiUart;
+// MidiUartOSXClass MidiUart;
 MidiUartOSXClass MidiUart2;
 
 char *file = NULL;
 
-void hexDump(uint8_t *data, uint16_t len) {
-  uint8_t cnt = 0;
-  for (int i = 0; i < len; i++) {
-    if (cnt == 0) {
-      printf("%.4x: ", i & 0xFFF0);
-    }
-    printf("%.2x ", data[i]);
-    cnt++;
-    if (cnt == 8) {
-      printf(" ");
-    }
-    if (cnt == 16) {
-      printf("\n");
-      cnt = 0;
-    }
-  }
-  if (cnt != 0) {
-    printf("\n");
-  }
-}
-
 bool printFirst = false;
 bool printSecond = true;
-
 
 int main(int argc, char *argv[]) {
   MidiUart.init(0, 0);
