4282ef6409cc48f42ab664abe9af045144398799
Manuel Odendahl <wesen@ruinwesen.com>
2011-07-02 13:12:27 +0200

    Remove unused cores

--- hardware/cores/mididuino-small/GUI_private.h
+++
@@ -1,4 +0,0 @@
-#ifndef GUI_PRIVATE_H__
-#define GUI_PRIVATE_H__
-
-#endif /* BLA */
--- hardware/cores/mididuino/USBMidiUart.h
+++
@@ -1,7 +0,0 @@
-#ifndef USBMIDI_UART_H__
-#define USBMIDI_UART_H__
-
-#include <inttypes.h>
-#include "MidiUart.h"
-#include "RingBuffer.h"
-#endif /* USBMIDI_UART_H__ */
