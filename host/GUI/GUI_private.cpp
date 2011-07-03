2ac6271c2dc570919bd0a280bc063d8172f73bd1
Manuel Odendahl <wesen@ruinwesen.com>
2011-07-03 16:18:23 +0200

    Added mock implementation for encoders and buttons on the host

--- host/GUI/GUI_private.cpp
+++ host/GUI/GUI_private.cpp
@@ -8,10 +8,52 @@
 EncodersClass Encoders;
 ButtonsClass Buttons;
 
+/** Mock encoder class **/
 #define ENCODER_NORMAL(i) (encoders[(i)].normal)
-#define ENCODER_SHIFT(i)  (encoders[(i)].shift)
 #define ENCODER_BUTTON(i) (encoders[(i)].button)
-#define ENCODER_BUTTON_SHIFT(i) (encoders[(i)].button_shift)
 
 EncodersClass::EncodersClass() {
+  clearEncoders();
+}
+
+void EncodersClass::clearEncoders() {
+  for (uint8_t i = 0; i < GUI_NUM_ENCODERS; i++) {
+    ENCODER_NORMAL(i) = ENCODER_BUTTON(i) = 0;
+  }
+}
+
+void EncodersClass::turnEncoder(uint8_t encoder, int i, bool button) {
+  if (button) {
+    ENCODER_BUTTON(encoder) = i;
+  } else {
+    ENCODER_NORMAL(encoder) = i;
+  }
+}
+
+/** Mock buttons class **/
+ButtonsClass::ButtonsClass() {
+  clearButtons();
+}
+
+void ButtonsClass::clearButtons() {
+  for (uint8_t i = 0; i < GUI_NUM_BUTTONS; i++) {
+    buttons[i].status = 0;
+    /* buttons are up and were up */
+    STORE_B_OLD(i, 1);
+    STORE_B_CURRENT(i, 1);
+  }
+}
+
+void ButtonsClass::pressButton(uint8_t button) {
+  STORE_B_CURRENT(button, 0);
+}
+
+void ButtonsClass::releaseButton(uint8_t button) {
+  STORE_B_CURRENT(button, 1);
+}
+
+void ButtonsClass::printButtons() {
+  for (uint8_t i = 0; i < GUI_NUM_BUTTONS; i++) {
+    printf("Button %d: %x\n", i, B_CURRENT(i));
+  }
 }
