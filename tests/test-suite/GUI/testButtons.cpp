58f9e7054167f5611ffa160a801fa72850b38922
Manuel Odendahl <wesen@ruinwesen.com>
2011-07-03 16:36:40 +0200

    Add buttons tests and encoder tests

--- tests/test-suite/GUI/testButtons.cpp
+++ tests/test-suite/GUI/testButtons.cpp
@@ -10,9 +10,9 @@
 #include <GUI.h>
 
 CRingBuffer<gui_event_t, 32> _eventRB;
+gui_event_t _event;
 
 bool TestButtonsHandler(gui_event_t *event) {
-  printf("got event from GUI\n");
   _eventRB.putp(event);
   return true;
 }
@@ -31,27 +31,91 @@ struct ButtonFixture {
 };
 
 
-TEST_F (ButtonFixture, TestButtons) {
+TEST_F (ButtonFixture, SingleButtonPressRelease) {
+  GUI_tick();
+
   CHECK(BUTTON_UP(Buttons.BUTTON1));
   CHECK(!BUTTON_DOWN(Buttons.BUTTON1));
   CHECK(BUTTON_UP(Buttons.BUTTON2));
 
+  /* press a button */
   Buttons.pressButton(Buttons.BUTTON1);
+  GUI_tick();
+  
+  CHECK(BUTTON_DOWN(Buttons.BUTTON1));
+  CHECK(!BUTTON_UP(Buttons.BUTTON1));
+  CHECK(BUTTON_UP(Buttons.BUTTON2));
+
+  CHECK_EQUAL(1, _eventRB.size());
+  _eventRB.getp(&_event);
+  CHECK(EVENT_PRESSED(&_event, Buttons.BUTTON1));
+
+  /* let the GUI tick without doing anything */
+  GUI_tick();
 
   CHECK(BUTTON_DOWN(Buttons.BUTTON1));
   CHECK(!BUTTON_UP(Buttons.BUTTON1));
   CHECK(BUTTON_UP(Buttons.BUTTON2));
-  CHECK(BUTTON_PRESSED(Buttons.BUTTON1));
-  CHECK(!BUTTON_RELEASED(Buttons.BUTTON1));
-  CHECK(!BUTTON_PRESSED(Buttons.BUTTON2));
-  CHECK(!BUTTON_RELEASED(Buttons.BUTTON2));
+  CHECK_EQUAL(0, _eventRB.size());
+  
+  /* release the button */
+  Buttons.releaseButton(Buttons.BUTTON1);
+  GUI_tick();
 
-  pollEventGUI();
-  GUI.loop();
+  CHECK(!BUTTON_DOWN(Buttons.BUTTON1));
+  CHECK(BUTTON_UP(Buttons.BUTTON1));
+  CHECK(BUTTON_UP(Buttons.BUTTON2));
+  CHECK_EQUAL(1, _eventRB.size());
+  _eventRB.getp(&_event);
+  CHECK(EVENT_RELEASED(&_event, Buttons.BUTTON1));
+}
+
+TEST_F (ButtonFixture, TwoButtonPressRelease) {
+  GUI_tick();
+
+  Buttons.pressButton(Buttons.BUTTON1);
+  GUI_tick();
+  
+  CHECK_EQUAL(1, _eventRB.size());
+  _eventRB.getp(&_event);
+  CHECK(EVENT_PRESSED(&_event, Buttons.BUTTON1));
+
+  GUI_tick();
+  CHECK_EQUAL(0, _eventRB.size());
+  GUI_tick();
+  CHECK_EQUAL(0, _eventRB.size());
+
+  Buttons.pressButton(Buttons.BUTTON2);
+
+  /* let the GUI tick without doing anything */
+  GUI_tick();
 
   CHECK_EQUAL(1, _eventRB.size());
+  _eventRB.getp(&_event);
+  CHECK(EVENT_PRESSED(&_event, Buttons.BUTTON2));
 
-  gui_event_t event;
-  _eventRB.getp(&event);
-  CHECK(EVENT_PRESSED(&event, Buttons.BUTTON1));
+  GUI_tick();
+  CHECK_EQUAL(0, _eventRB.size());
+  GUI_tick();
+  CHECK_EQUAL(0, _eventRB.size());
+
+  /* release the button */
+  Buttons.releaseButton(Buttons.BUTTON1);
+  GUI_tick();
+
+  CHECK_EQUAL(1, _eventRB.size());
+  _eventRB.getp(&_event);
+  CHECK(EVENT_RELEASED(&_event, Buttons.BUTTON1));
+
+  GUI_tick();
+  CHECK_EQUAL(0, _eventRB.size());
+  GUI_tick();
+  CHECK_EQUAL(0, _eventRB.size());
+
+  Buttons.releaseButton(Buttons.BUTTON2);
+  GUI_tick();
+
+  CHECK_EQUAL(1, _eventRB.size());
+  _eventRB.getp(&_event);
+  CHECK(EVENT_RELEASED(&_event, Buttons.BUTTON2));
 }
--- tests/test-suite/GUI/testEncoders.cpp
+++ tests/test-suite/GUI/testEncoders.cpp
@@ -9,21 +9,58 @@
  * - CHECK_EQUAL(expected, actual)
  */
 
-TEST (TestSingleEncoder) {
-  CHECK(false);
+struct EncoderPageFixture {
+  EncoderPageFixture() : page(&encs[0], &encs[1], &encs[2], &encs[3]) {
+  }
 
-  RangeEncoder enc;
-  EncoderPage page(&enc);
+  ~EncoderPageFixture() {
+  }
 
+  RangeEncoder encs[4];
+  EncoderPage page;
+};
+
+TEST_F (EncoderPageFixture, TestSingleEncoder) {
+  CHECK_EQUAL(0, encs[0].getValue());
+  CHECK_EQUAL(0, encs[1].getValue());
+  CHECK_EQUAL(0, encs[2].getValue());
+  CHECK_EQUAL(0, encs[3].getValue());
+  
   Encoders.turnEncoder(0, 1);
   page.update();
-}
 
+  CHECK_EQUAL(1, encs[0].getValue());
+  CHECK_EQUAL(0, encs[1].getValue());
+  CHECK_EQUAL(0, encs[2].getValue());
+  CHECK_EQUAL(0, encs[3].getValue());
 
-TEST (TestCurrentPageEncoders) {
-  RangeEncoder enc;
-  EncoderPage page(&enc);
-
-  Encoders.turnEncoder(0, 1);
+  Encoders.turnEncoder(0, 3);
   page.update();
+
+  CHECK_EQUAL(4, encs[0].getValue());
+
+  Encoders.turnEncoder(0, -3);
+  page.update();
+
+  CHECK_EQUAL(1, encs[0].getValue());
+
+  Encoders.turnEncoder(0, 100);
+  page.update();
+
+  CHECK_EQUAL(101, encs[0].getValue());
+
+  Encoders.turnEncoder(0, 30);
+  page.update();
+
+  CHECK_EQUAL(127, encs[0].getValue());
+
+  Encoders.turnEncoder(0, -70);
+  page.update();
+
+  CHECK_EQUAL(57, encs[0].getValue());
+
+  Encoders.turnEncoder(0, -70);
+  page.update();
+
+  CHECK_EQUAL(0, encs[0].getValue());
 }
