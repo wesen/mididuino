545520f096a4fb2656ed426ac2917e135e6bc3e6
Manuel Odendahl <wesen@ruinwesen.com>
2011-07-04 22:49:15 +0200

    Convert misc stuff to use Platform.h

--- hardware/libraries/Profiler/Profiler.h
+++ hardware/libraries/Profiler/Profiler.h
@@ -1,7 +1,14 @@
+/*
+ * MidiCtrl - Simple Profiler to benchmark embedded midi ctrl code
+ *
+ * (c) 2009 - 2011 - Manuel Odendahl - wesen@ruinwesen.com
+ */
+
 #ifndef PROFILER_H__
 #define PROFILER_H__
 
-#include "WProgram.h"
+#include "PlatformConfig.h"
+#include "Task.hh"
 
 #ifndef HOST_MIDIDUINO
 void enableProfiling();
