8efdc53c45980e21e8c1b60acf04e1a261f9b356
Manuel Odendahl <wesen@ruinwesen.com>
2012-06-12 15:22:56 +0200

    Add new weak function callbacks for pitchwheel, aftertouch and program changes:
    - onPitchWheel, onPitchWheel2
    - onAfterTouch, onAfterTouch2
    - onProgramChange, onProgramChange2

--- hardware/cores/minicommand2/weak_funcs.cpp
+++ hardware/cores/minicommand2/weak_funcs.cpp
@@ -26,6 +26,19 @@ void __attribute__((weak)) onNoteOff(uint8_t *msg) {
 void __attribute__((weak)) onControlChange(uint8_t *msg) {
 }
 
+void __attribute__((weak)) onProgramChange(uint8_t *msg) {
+}
+
+void __attribute__((weak)) onChannelPressure(uint8_t *msg) {
+}
+
+void __attribute__((weak)) onAfterTouch(uint8_t *msg) {
+}
+
+void __attribute__((weak)) onPitchWheel(uint8_t *msg) {
+}
+
+/* MIDI 2 Callbacks */
 void __attribute__((weak)) onNoteOn2(uint8_t *msg) {
 }
 
@@ -33,6 +46,18 @@ void __attribute__((weak)) onNoteOff2(uint8_t *msg) {
 }
 
 void __attribute__((weak)) onControlChange2(uint8_t *msg) {
+}
+
+void __attribute__((weak)) onProgramChange2(uint8_t *msg) {
+}
+
+void __attribute__((weak)) onChannelPressure2(uint8_t *msg) {
+}
+
+void __attribute__((weak)) onAfterTouch2(uint8_t *msg) {
+}
+
+void __attribute__((weak)) onPitchWheel2(uint8_t *msg) {
 }
 
 
@@ -48,6 +73,8 @@ bool __attribute__((weak)) handleEvent(gui_event_t *evt) {
 
 class DefaultCallbacks : public MidiCallback, public ClockCallback {
 public:
+
+  /* callbacks on first midi interface */
   void onNoteOn(uint8_t *msg) {
     ::onNoteOn(msg);
   }
@@ -57,6 +84,20 @@ public:
   void onControlChange(uint8_t *msg) {
     ::onControlChange(msg);
   }
+  void onProgramChange(uint8_t *msg) {
+    ::onProgramChange(msg);
+  }
+  void onAfterTouch(uint8_t *msg) {
+    ::onAfterTouch(msg);
+  }
+  void onChannelPressure(uint8_t *msg) {
+    ::onChannelPressure(msg);
+  }
+  void onPitchWheel(uint8_t *msg) {
+    ::onPitchWheel(msg);
+  }
+
+  /* callbacks on second midi interface */
   void onNoteOn2(uint8_t *msg) {
     ::onNoteOn2(msg);
   }
@@ -66,6 +107,20 @@ public:
   void onControlChange2(uint8_t *msg) {
     ::onControlChange2(msg);
   }
+  void onProgramChange2(uint8_t *msg) {
+    ::onProgramChange2(msg);
+  }
+  void onAfterTouch2(uint8_t *msg) {
+    ::onAfterTouch2(msg);
+  }
+  void onChannelPressure2(uint8_t *msg) {
+    ::onChannelPressure2(msg);
+  }
+  void onPitchWheel2(uint8_t *msg) {
+    ::onPitchWheel2(msg);
+  }
+
+  /* midi clock callbacks */
   void on16Callback(uint32_t pos) {
     ::on16Callback(pos);
   }
@@ -77,9 +132,18 @@ public:
     Midi.addOnNoteOnCallback(this, (midi_callback_ptr_t)&DefaultCallbacks::onNoteOn);
     Midi.addOnNoteOffCallback(this, (midi_callback_ptr_t)&DefaultCallbacks::onNoteOff);
     Midi.addOnControlChangeCallback(this, (midi_callback_ptr_t)&DefaultCallbacks::onControlChange);
+    Midi.addOnProgramChangeCallback(this, (midi_callback_ptr_t)&DefaultCallbacks::onProgramChange);
+    Midi.addOnAfterTouchCallback(this, (midi_callback_ptr_t)&DefaultCallbacks::onAfterTouch);
+    Midi.addOnChannelPressureCallback(this, (midi_callback_ptr_t)&DefaultCallbacks::onChannelPressure);
+    Midi.addOnPitchWheelCallback(this, (midi_callback_ptr_t)&DefaultCallbacks::onPitchWheel);
+
     Midi2.addOnNoteOnCallback(this, (midi_callback_ptr_t)&DefaultCallbacks::onNoteOn2);
     Midi2.addOnNoteOffCallback(this, (midi_callback_ptr_t)&DefaultCallbacks::onNoteOff2);
     Midi2.addOnControlChangeCallback(this, (midi_callback_ptr_t)&DefaultCallbacks::onControlChange2);
+    Midi2.addOnProgramChangeCallback(this, (midi_callback_ptr_t)&DefaultCallbacks::onProgramChange2);
+    Midi2.addOnAfterTouchCallback(this, (midi_callback_ptr_t)&DefaultCallbacks::onAfterTouch2);
+    Midi2.addOnChannelPressureCallback(this, (midi_callback_ptr_t)&DefaultCallbacks::onChannelPressure2);
+    Midi2.addOnPitchWheelCallback(this, (midi_callback_ptr_t)&DefaultCallbacks::onPitchWheel2);
   }
 
   void setupClockCallbacks() {
